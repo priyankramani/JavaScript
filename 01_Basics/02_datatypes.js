bbcf785cd0c9e01f62d02f1288ebb3893886c053
priyankramani <priyankramani301@gmail.com>
2025-12-24 14:06:28 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+let name = "Priyank"
+let age = 21
+let bool = false
+
+/* Types : 
+1. number => 2^53
+2. bigint
+3.string => ""
+4. boolean => true/false
+5. null => standalone value
+6. undefined => value is not assigned
+7. symbol => unique
+8. object
+*/
+
+console.log(typeof("Priyank")) // string
+console.log(typeof null) // obejct
+console.log(typeof undefined) // undefined
