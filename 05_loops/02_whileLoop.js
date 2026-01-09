ccdd6a2e5398461ececb0ca8282592dcc562c074
priyankramani <priyankramani301@gmail.com>
2026-01-09 16:20:36 +0000

    While loop

---
+++ 05_loops/02_whileLoop.js
@@ -0,0 +1,13 @@
+// while loop
+let i = 1;
+while(i <= 10){
+    console.log(`Value of i is ${i}`);
+    i+=2;
+}
+
+// do-while loop : loop always run for one time because of we are checking condition at last
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+} while(score <= 10);
